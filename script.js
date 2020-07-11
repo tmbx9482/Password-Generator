ba5a6176f8ad849550426286a68e13d582bbf30f
Troy Berentsen <troyberentsen94@gmail.com>
2020-07-11 09:52:16 -0400

    needed to make last min changes

--- script.js
+++ script.js
@@ -102,29 +102,3 @@ function generatePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
